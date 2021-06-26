7d4784f91c70ddfd2219557d1c2d734d17ccab33
Karol <kalix321@gmail.com>
2021-06-26 17:39:06 +0200

    finito

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const converter = document.querySelector('#converter');
-const result = document.querySelector('result');
+const result = document.querySelector('.result');
 const convBtn = document.querySelector('.conv');
 const resetBtn = document.querySelector('.reset');
 const changeBtn = document.querySelector('.change');
@@ -7,3 +7,46 @@ const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 let fahrenheit;
 let celsius;
+
+const swap = () => {
+    if(one.innerText==='°C') {
+        one.innerText ='°F';
+        two.innerText ='°C';
+        result.innerText = '';
+    } else {
+        one.innerText ='°C';
+        two.innerText ='°F';
+        result.innerText = '';
+    }
+};
+
+const celToFahr = () => {
+    fahrenheit = converter.value * 1.8 + 32;
+    result.innerText = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
+    converter.value = '';
+};
+const fahrToCel = () => {
+    celsius = (converter.value  - 32) / 1.8;
+    result.innerText = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+    converter.value = '';
+};
+
+const conversion = () => {
+    if (converter.value !== '') {
+        if (one.innerText === '°C') {
+            celToFahr();
+        } else {
+            fahrToCel();
+        }
+    } else {
+        result.innerText = 'Musisz wpisać jakąś liczbę'
+    }
+}
+
+const reset = () => {
+    converter.value = '';
+    result.innerText = '';
+}
+convBtn.addEventListener('click', conversion);
+changeBtn.addEventListener('click', swap);
+resetBtn.addEventListener('click', reset);
